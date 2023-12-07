30956239a3167b39ee69557de1df331db6e12ed4
Leticia <92949025+leticiaesttrela@users.noreply.github.com>
2023-12-07 08:07:14 -0300

    Update singleton.rb

--- singleton/singleton.rb
+++ singleton/singleton.rb
@@ -1,14 +1,13 @@
 ##AppConfig armazena configurações globais do aplicativo, como a chave da API e o modo de depuração.
 ##a instância única é acessada através do método de classe instance.
-##isso permite que as configurações sejam compartilhadas em todo o código, e as alterações
-##feitas em uma parte do programa são refletidas em outras.
 
 
 class AppConfig
+#define dois atributos de instância (api_key e debug_mode
   attr_accessor :api_key, :debug_mode
-
+#construtor privado que impede que novas instancias de classes sejam diretamentes criadas
   private_class_method :new
-
+#usada para armazenar a única instância da classe.
   @@instance = nil
 
   def self.instance
@@ -16,8 +15,9 @@ class AppConfig
   end
 end
 
-##uso do Singleton para configurações
-app_config = AppConfig.instance
+#obtém a instância única da classe 
+app_config = AppConfig.instance 
+#define o valor da propriedade api_key na instância única.
 app_config.api_key = "my_api_key"
 app_config.debug_mode = true
 
